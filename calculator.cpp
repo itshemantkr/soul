ac658795a5408259a54c6cdc47931c811e5ccf19
rishabhkumar112 <34334329+rishabhkumar112@users.noreply.github.com>
2018-01-23 18:55:26 +0530

    create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,88 @@
+#include <iostream.h>
+#include <conio.h>
+#include <math.h>
+
+long int fact(int a)
+{
+	if(a==1 || a==0)
+		return 1;
+	else 
+		return a*fact(a-1);
+}
+
+
+int main ()
+{       
+	clrscr();
+	cout<<"\t\t\t\tCALCULATOR";
+	double a, b;
+	int menu;
+	char option;
+	abc:
+	cout<<"\n\n\t\t\t********** MENU **********";
+	cout<<"\n1. Addition";
+	cout<<"\n2. Subtraction";
+	cout<<"\n3. Multiplication";
+	cout<<"\n4. Division";
+	cout<<"\n5. Sum upto n number ";
+	cout<<"\n6. Sum of squares upto n number ";
+	cout<<"\n7. Logarithm ";
+	cout<<"\n8. Factorial ";
+	do
+	{
+		cout<<"\nEnter operation number from MENU: ";
+		cin>>menu;
+		switch(menu)
+		{
+			case 1:
+			cout<<"Enter two numbers: "<<"\n";
+			cin>>a>>b;
+			cout<<a<<" + "<<b<<" = "<<a+b<<"\n";
+			break;
+			case 2:
+			cout<<"Enter two numbers: "<<"\n";
+			cin>>a>>b;
+			cout<<a<<" - "<<b<<" = "<<a-b<<"\n";
+			break;
+			case 3:
+			cout<<"Enter two numbers: "<<"\n";
+			cin>>a>>b;
+			cout<<a<<" x "<<b<<" = "<<a*b<<"\n";
+			break;
+			case 4:
+			cout<<"Enter two numbers: "<<"\n";
+			cin>>a>>b;
+			cout<<a<<" / "<<b<<" = "<<a/b<<"\n";
+			break;
+			case 5: cout<<"\nEnter the number upto which you want the sum: ";
+			cin>>a;
+			cout<<(a*(a+1))/2;
+			break;
+			case 6: cout<<"\nEnter the number upto which you want the sum of squares: ";
+			cin>>a;
+			cout<<(a*(a+1)*(2*a+1))/6;
+			break;
+			case 7:cout<<"\nEnter the number to find logarithm of the number to the base e: ";
+			cin>>a;
+			cout<<log(a);
+			break;
+			case 8: cout<<"\nEnter the number whose factorial you want to calculate: ";
+			cin>>a;
+			cout<<"\n"<<fact(a);
+			break;
+			default:cout<<"Wrong option chosen :::\n";
+			break;
+		}
+		cout<<"\nWant to do more calculations: (Press y or Y for yes): ";
+		cin>>option;
+		clrscr();
+		if(option=='y' || option=='Y')
+		goto abc;
+		else
+		goto pqr;
+	}while(option=='y' || option=='Y');
+	pqr:
+	cout<<"\n\n\t\t**** PRESS ANY KEY TO EXIT **** ";
+	getch();
+	return 0;
+}
